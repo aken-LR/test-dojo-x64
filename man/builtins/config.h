05e8c899b42768a8e259e659ebba1a007e3820d0
aken <19864568839@163.com>
2025-07-23 15:10:16 +0800

    update..

--- man/builtins/config.h
+++ man/builtins/config.h
@@ -1,7 +1,6 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-/* challenge_struct 中用到的 BUILTIN_ENABLED 宏 */
 #define BUILTIN_ENABLED 0x1
 
 #endif /* _CONFIG_H_ */
--- man/builtins/variables.h
+++ man/builtins/variables.h
@@ -9,9 +9,8 @@ typedef struct shell_var {
   struct shell_var *next;
 } SHELL_VAR;
 
-/* 提供代码中用到的函数原型，以便链接器能找到它们 */
 SHELL_VAR *find_variable(char*);
 char *get_variable_value(SHELL_VAR*);
-//int bind_global_variable(char*, char*, int); // 如果你的代码解注释了 bind_global_variable，也需要这个原型
+//int bind_global_variable(char*, char*, int); 
 
 #endif /* _VARIABLES_H_ */
